33e60fa09bed1bd2cc06793e661dd9bb71224d56
kamilwadih <720013@student.dusd.net>
2024-05-10 14:18:58 -0700

    update

--- Ghosted-away-main/Assets/UnityTechnologies/3DBeginnerComplete/Scripts/PlayerMovement.cs
+++ Ghosted-away-main/Assets/UnityTechnologies/3DBeginnerComplete/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public InputAction MoveAction;
-
+    
     public float turnSpeed = 20f;
 
     Animator m_Animator;
@@ -15,32 +15,30 @@ public class PlayerMovement : MonoBehaviour
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
-    void Start()
+    void Start ()
     {
-        m_Animator = GetComponent<Animator>();
-        m_Rigidbody = GetComponent<Rigidbody>();
-        m_AudioSource = GetComponent<AudioSource>();
-
-        // Initialize Input System
-        MoveAction = new InputAction(binding: "<Gamepad>/leftStick");
+        m_Animator = GetComponent<Animator> ();
+        m_Rigidbody = GetComponent<Rigidbody> ();
+        m_AudioSource = GetComponent<AudioSource> ();
+        
         MoveAction.Enable();
     }
 
-    void FixedUpdate()
+    void FixedUpdate ()
     {
         var pos = MoveAction.ReadValue<Vector2>();
-
+        
         float horizontal = pos.x;
         float vertical = pos.y;
-
+        
         m_Movement.Set(horizontal, 0f, vertical);
-        m_Movement.Normalize();
+        m_Movement.Normalize ();
 
-        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
-        m_Animator.SetBool("IsWalking", isWalking);
-
+        m_Animator.SetBool ("IsWalking", isWalking);
+        
         if (isWalking)
         {
             if (!m_AudioSource.isPlaying)
@@ -50,16 +48,16 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            m_AudioSource.Stop();
+            m_AudioSource.Stop ();
         }
 
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation(desiredForward);
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation (desiredForward);
     }
 
-    void OnAnimatorMove()
+    void OnAnimatorMove ()
     {
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
-        m_Rigidbody.MoveRotation(m_Rotation);
+        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation (m_Rotation);
     }
 }
