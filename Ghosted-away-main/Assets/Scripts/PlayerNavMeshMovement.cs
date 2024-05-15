3b263183db1bbecab79c341e8141b84ab5453276
kamilwadih <720013@student.dusd.net>
2024-05-15 14:07:01 -0700

    save

--- Ghosted-away-main/Assets/Scripts/PlayerNavMeshMovement.cs
+++ Ghosted-away-main/Assets/Scripts/PlayerNavMeshMovement.cs
@@ -7,11 +7,12 @@ public class PlayerNavMeshMovement : MonoBehaviour
 {
     public float movementSpeed = 30f;
     private NavMeshAgent navMeshAgent;
-    private Vector3 lastTargetPosition;
+    private Animator animator;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -19,12 +20,18 @@ public class PlayerNavMeshMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        if (Mathf.Abs(horizontalInput) > 0.1f || Mathf.Abs(verticalInput) > 0.1f)
+        Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        Vector3 moveVelocity = movementDirection * movementSpeed;
+
+        if (moveVelocity.magnitude > 0.1f)
         {
-            Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-            Vector3 targetPosition = transform.position + movementDirection * movementSpeed * Time.deltaTime;
-            navMeshAgent.SetDestination(targetPosition);
-            lastTargetPosition = targetPosition;
+            navMeshAgent.SetDestination(transform.position + moveVelocity * Time.deltaTime);
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            navMeshAgent.ResetPath();
+            animator.SetBool("IsWalking", false);
         }
     }
 }
